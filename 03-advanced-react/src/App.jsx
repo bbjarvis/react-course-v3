d0798b437a55abb5fd4424614038f3ae553e54e2
Brett Jarvis <brettbjarvis@gmail.com>
2023-02-16 13:43:56 -0800

    init clone, add 02-multiple-returns-fect-data from previous tutorial

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,12 @@
+import Setup from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
+import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data'
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React</h2>
+      <Setup />
+      <Final />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,52 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
-};
-export default MultipleReturnsFetchData;
+  const [loading, setLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch(url)
+        if (!res.ok) {
+          setIsError(true)
+          setLoading(false)
+          return
+        }
+        const user = await res.json()
+        console.log(user)
+        setUser(user)
+      } catch (error) {
+        console.log(error)
+        setIsError(true)
+      }
+      setLoading(false)
+    }
+    fetchUser()
+  }, [])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+  if (isError) {
+    return <h1>Error</h1>
+  }
+  const { name, company, bio, blog, login, avatar_url, html_url } = user
+
+  return (
+    <div>
+      <a href={html_url}>
+        <h1>{name}</h1>
+      </a>
+      <a href={html_url}>
+        <img src={avatar_url} alt={login}></img>
+      </a>
+      <a href={blog}>
+        <h2> Works at {company}</h2>
+      </a>
+      <h3>{bio}</h3>
+    </div>
+  )
+}
+export default MultipleReturnsFetchData
