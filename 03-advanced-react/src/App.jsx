6f07e6378494d25fe86e478b10a0e519bc5b7107
Brett Jarvis <brettbjarvis@gmail.com>
2023-02-23 13:19:16 -0800

    add 04-short-circuit-verview fxnality

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Setup from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
-import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data'
+import Setup from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview'
+import Final from './tutorial/03-conditional-rendering/final/04-short-circuit-overview'
 function App() {
   return (
     <div className='container'>
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
-};
-export default ShortCircuitOverview;
+  // falsey?
+  const [text, setText] = useState('')
+  // truthey
+  const [name, setName] = useState('susan')
+
+  const codeExample = text || 'hello world'
+
+  return (
+    <div>
+      <h4>Falsey OR : {text || 'hello world'}</h4>
+      <h4>Falsey AND : {text && 'hello world'}</h4>
+      <h4>Falsey OR : {name || 'hello world'}</h4>
+      <h4>Falsey AND : {name && 'hello world'}</h4>
+      {codeExample}
+    </div>
+  )
+}
+export default ShortCircuitOverview
