690703b986981308f2ef40bf432965a09f9acfc0
Brett Jarvis <brettbjarvis@gmail.com>
2023-03-10 12:44:12 -0800

    move alert into own component

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react'
-
+const message = 'hello world'
 const ToggleChallenge = () => {
   const [toggle, setToggle] = useState(false)
-  const message = 'hello world'
 
   return (
     <div>
       <button className='btn' onClick={() => setToggle(!toggle)}>
         Toggle Alert
       </button>
-      {toggle && <div className='alert alert-danger'>{message}</div>}
+      {toggle && <Alert />}
     </div>
   )
 }
 
+const Alert = () => {
+  return <div className='alert alert-danger'>{message}</div>
+}
 export default ToggleChallenge
