13cc6c5b7a99563d89452ffa6189e7d413fc902e
Brett Jarvis <brettbjarvis@gmail.com>
2023-03-13 16:14:02 -0700

    init 07-user-challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Setup from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
-import Final from './tutorial/03-conditional-rendering/final/06-toggle-challenge'
+import Setup from './tutorial/03-conditional-rendering/starter/07-user-challenge'
+import Final from './tutorial/03-conditional-rendering/final/07-user-challenge'
 function App() {
   return (
     <div className='container'>
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,34 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+  const [user, setUser] = useState(null)
+  const userObj = { username: 'Brett' }
+
+  const Login = () => {
+    return (
+      <div>
+        <h4>Hello There, {user.username}</h4>
+
+        <button className='btn' onClick={() => setUser(null)}>
+          Logout
+        </button>
+      </div>
+    )
+  }
+
+  const Logout = () => {
+    return (
+      <div>
+        <h4>Please Login</h4>
+
+        <button className='btn' onClick={() => setUser(userObj)}>
+          Login
+        </button>
+      </div>
+    )
+  }
+
+  return <div>{user ? <Login /> : <Logout />}</div>
+}
+
+export default UserChallenge
