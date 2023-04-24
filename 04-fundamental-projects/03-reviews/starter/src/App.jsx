955568de6a6f45ddbf000e06384b539e14d28a5b
Brett Jarvis <brettbjarvis@gmail.com>
2023-04-24 10:19:34 -0700

    initial set up

--- 04-fundamental-projects/03-reviews/starter/src/App.jsx
+++ 04-fundamental-projects/03-reviews/starter/src/App.jsx
@@ -1,13 +1,34 @@
+import reviews from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
 const App = () => {
   return (
     <main>
-      <section>
-        <h2 className='title'>
-          Our Reviews
-          <h3 className='underline'></h3>
-        </h2>
-      </section>
-      <container className='container'></container>
+      <article className='review'>
+        <div className='review-hover'>
+          <div className='img-container'>
+            <img className='person-img' src={reviews[0].image}></img>
+            <span className='quote-icon'>
+              <FaQuoteRight />
+            </span>
+          </div>
+
+          <h4 className='author'>{reviews[0].name}</h4>
+          <p className='job'>{reviews[0].job}</p>
+          <p className='info'>{reviews[0].text}</p>
+        </div>
+        <div>
+          <button className='prev-btn'>
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn'>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className='btn'>
+          <div className='btn-hover'>Surprise Me</div>
+        </button>
+      </article>
     </main>
   )
 }
