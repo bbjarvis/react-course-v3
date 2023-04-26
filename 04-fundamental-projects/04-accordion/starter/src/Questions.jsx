28c87692d29e31ff0a53459011432214f0d26949
Brett Jarvis <brettbjarvis@gmail.com>
2023-04-26 15:16:20 -0700

    map questions in Questions.jsx

--- 04-fundamental-projects/04-accordion/starter/src/Questions.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/Questions.jsx
@@ -2,15 +2,21 @@ import questions from './data'
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 const Questions = () => {
   return (
-    <div className='question'>
-      <header>
-        <h5>{questions[0].title}</h5>
-        <button className='question-btn'>
-          <AiOutlinePlus />
-        </button>
-      </header>
-      <p>{questions[0].info}</p>
-    </div>
+    <>
+      {questions.map((question) => {
+        return (
+          <div className='question'>
+            <header>
+              <h5>{questions[0].title}</h5>
+              <button className='question-btn'>
+                <AiOutlinePlus />
+              </button>
+            </header>
+            <p>{questions[0].info}</p>
+          </div>
+        )
+      })}
+    </>
   )
 }
 export default Questions
