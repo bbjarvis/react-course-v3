8ddbaf9c2fff454e22f1d3b68e3e010b3f9522e7
Brett Jarvis <brettbjarvis@gmail.com>
2023-04-26 14:44:29 -0700

    add icons

--- 04-fundamental-projects/04-accordion/starter/src/App.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/App.jsx
@@ -1,13 +1,17 @@
 import questions from './data'
-
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 const App = () => {
   return (
     <main>
       <section className='container'>
         <h1>Questions</h1>
         <div className='question'>
-          <h5>{questions[0].title}</h5>
-
+          <header>
+            <h5>{questions[0].title}</h5>
+            <button className='question-btn'>
+              <AiOutlinePlus />
+            </button>
+          </header>
           <p>{questions[0].info}</p>
         </div>
       </section>
