d62ddaf1e589838943edb4bd318868fb5922213d
Brett Jarvis <brettbjarvis@gmail.com>
2023-04-26 14:29:47 -0700

    initial look

--- 04-fundamental-projects/04-accordion/starter/src/App.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/App.jsx
@@ -1,4 +1,17 @@
+import questions from './data'
+
 const App = () => {
-  return <h2>Accordion Starter</h2>;
-};
-export default App;
+  return (
+    <main>
+      <section className='container'>
+        <h1>Questions</h1>
+        <div className='question'>
+          <h5>{questions[0].title}</h5>
+
+          <p>{questions[0].info}</p>
+        </div>
+      </section>
+    </main>
+  )
+}
+export default App
