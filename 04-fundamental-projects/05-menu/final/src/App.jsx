5fbd15bf5c78976454f70f0c0afa44c175db90ca
Brett Jarvis <brettbjarvis@gmail.com>
2023-05-02 12:46:58 -0700

    center title

--- 04-fundamental-projects/05-menu/final/src/App.jsx
+++ 04-fundamental-projects/05-menu/final/src/App.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import Title from './Title';
-import items from './data';
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import Title from './Title'
+import items from './data'
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
 
   const filterItems = (category) => {
     if (category === 'all') {
-      setMenuItems(items);
-      return;
+      setMenuItems(items)
+      return
     }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
 
   return (
     <main>
@@ -26,7 +26,7 @@ function App() {
         <Menu items={menuItems} />
       </section>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -1,4 +1,14 @@
+import React, { useState } from 'react'
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
-};
-export default App;
+  return (
+    <main>
+      <section classname='menu'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+        </div>
+      </section>
+    </main>
+  )
+}
+export default App
