a08e1231f13a558ab08eec575b527416445cbf53
Brett Jarvis <brettbjarvis@gmail.com>
2023-05-08 09:46:51 -0700

    add menu container

--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -4,6 +4,7 @@ const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 const App = () => {
   const [categories, setCategories] = useState(allCategories)
+  const [menuItems, setMenuItems] = useState(items)
   return (
     <main>
       <section classname='menu'>
@@ -17,6 +18,7 @@ const App = () => {
           })}
         </container>
       </section>
+      <section className='section-center'>{menuItems[0].title}</section>
     </main>
   )
 }
