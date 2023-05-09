85c16bfab65e290836a94841e1dee8986c691f2c
Brett Jarvis <brettbjarvis@gmail.com>
2023-05-09 10:34:49 -0700

    map through menuItems

--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -19,16 +19,20 @@ const App = () => {
         </container>
       </section>
       <section className='section-center'>
-        <article className='menu-item'>
-          <img src={menuItems[0].img} className='img'></img>
-          <div className='item-info'>
-            <header>
-              <h5>{menuItems[0].title}</h5>
-              <span className='item-price'>{menuItems[0].price}</span>
-            </header>
-            <p className='item-text'>{menuItems[0].desc}</p>
-          </div>
-        </article>
+        {menuItems.map((menuItem) => {
+          return (
+            <article className='menu-item'>
+              <img src={menuItem.img} className='img'></img>
+              <div className='item-info'>
+                <header>
+                  <h5>{menuItems[0].title}</h5>
+                  <span className='item-price'>{menuItem.price}</span>
+                </header>
+                <p className='item-text'>{menuItem.desc}</p>
+              </div>
+            </article>
+          )
+        })}
       </section>
     </main>
   )
